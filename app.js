5b86db35019f15211d33aa80d3d559647f89e2e8
DESKTOP-UEHUSN4\ICUNIX <azxca1731@naver.com>
2017-11-15 08:49:58 +0900

    nav - var pug 로 변경 , sign-in pug로 변경해서 구현 성공

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var passport = require('passport');
 var app = express();
 
 // view engine setup
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+require('./routes/auth')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
