4ce0936a3c33cf1674b7bba20ccc4f56bd6c9b0e
DESKTOP-UEHUSN4\ICUNIX <ponyong@naver.com>
2017-12-06 22:04:21 +0900

    For Deploy

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.locals.querystring = require('querystring');
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
 const connStr = 'mongodb://localhost/admin';
 // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
-// const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+const connStr = (process.env.NODE_ENV == 'production') ?
+  'mongodb://dbuser1:gurwls123!@@ds033186.mlab.com:33186/mydb' :
+  'mongodb://localhost/3000';
 mongoose.connect(connStr, {useMongoClient: true });
 mongoose.connection.on('error', console.error);
 
